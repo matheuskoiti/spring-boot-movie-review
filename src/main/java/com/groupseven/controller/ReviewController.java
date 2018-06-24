38b262d2ccef58e34a4c21f2400904433391e931
Matheus Koiti <koiti.silva@gmail.com>
2018-06-24 17:29:23 -0300

    adding image urls

--- src/main/java/com/groupseven/controller/ReviewController.java
+++ src/main/java/com/groupseven/controller/ReviewController.java
@@ -5,7 +5,6 @@ import com.groupseven.model.Review;
 import com.groupseven.repository.MovieRepository;
 import com.groupseven.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
